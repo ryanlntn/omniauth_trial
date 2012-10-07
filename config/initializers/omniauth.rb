9d79f4ca1f18bf0dc9493de291b4c0ae9f10fc86
Ryan Linton <ryanlntn@gmail.com>
2012-10-07 11:42:22 -0700

    Add user scaffolding

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	# provider :developer unless Rails.env.production?
-	provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+	provider :twitter, ENV['6agmlbHSZV5mUsaAfFd2Yw'], ENV['iQbRMYfukBeGGuVsk2VPECe2GMEa4EPAfxU3x31rO4']
 end
