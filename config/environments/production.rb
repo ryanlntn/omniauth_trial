5572e71136b5357d197d6bb282f2e402ac13cdec
Ryan Linton <ryanlntn@gmail.com>
2012-10-07 14:28:19 -0700

    cc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ OmniauthTrial::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  ENV['TWITTER_KEY'] = '6agmlbHSZV5mUsaAfFd2Yw'
-  ENV['TWITTER_SECRET'] = 'iQbRMYfukBeGGuVsk2VPECe2GMEa4EPAfxU3x31rO4'
 end
+
+ENV['TWITTER_KEY'] = '6agmlbHSZV5mUsaAfFd2Yw'
+ENV['TWITTER_SECRET'] = 'iQbRMYfukBeGGuVsk2VPECe2GMEa4EPAfxU3x31rO4'
