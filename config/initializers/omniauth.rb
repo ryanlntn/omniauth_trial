aa4e3806d873306ffd164e085db7b5411f032b84
Ryan Linton <ryanlntn@gmail.com>
2012-10-07 14:37:21 -0700

    cc

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, '6agmlbHSZV5mUsaAfFd2Yw', 'iQbRMYfukBeGGuVsk2VPECe2GMEa4EPAfxU3x31rO4'
 end
